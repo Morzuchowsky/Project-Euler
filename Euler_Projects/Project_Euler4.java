85afd87e9ca992c01e035db5eda939ab9d54548e
Adam <111274172+Morzuchowsky@users.noreply.github.com>
2023-06-18 12:53:10 +0200

    Problem 4 of Project Euler
    I am adding the solution to Problem 4 of Project Euler.

---
+++ Euler_Projects/Project_Euler4.java
@@ -0,0 +1,35 @@
+public class Project_Euler4 {
+    public static void main(String[] args) {
+        //A palindromic number reads the same both ways.
+        // The largest palindrome made from the product of two-digit numbers is 9009 == 91 X 99
+        //Find the largest palindrome made from the product of two 3-digit numbers.
+
+        int largestPalindrome = 0;
+        for (int i = 999; i > 99; i--) {
+            for (int l = 999; l > 99; l--) {
+                int palindromeNumber = i * l;
+                String intToString = String.valueOf(palindromeNumber);
+                char[] stringToCharArray = intToString.toCharArray();
+
+                boolean isPalindrome = true;
+
+
+                for (int k = 0; k < stringToCharArray.length / 2; k++) {
+                    char firstChar = stringToCharArray[k];
+                    char lastChar = stringToCharArray[stringToCharArray.length - k - 1];
+                    if (firstChar != lastChar) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome && palindromeNumber > largestPalindrome) {
+                    largestPalindrome = palindromeNumber;
+                }
+            }
+        }
+        System.out.println("This is Palindrome number: " + largestPalindrome);
+    }
+}
+
+
+
